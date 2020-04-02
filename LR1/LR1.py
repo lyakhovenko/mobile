7a350af2b88d1855c24df8e76431f4bb05892cc9
Julia Lyakhovenko <50697513+lyakhovenko@users.noreply.github.com>
2020-04-02 16:08:30 +0300

    Add files via upload

--- LR1/LR1.py
+++ LR1/LR1.py
@@ -8,21 +8,23 @@ with open('data.csv') as data_file:
         list_1.append(a)
 
 number = '915783624'
-k = 2
-s = 1
-minut_call = 0
+k = 1
+s = 5
+minut_call = 0.0
 coat_sms = 0
 
 for i in range(9):
     if number in list_1[i][1]:
-        minut_call += float(list_1[i][3])
-        coat_sms += float(list_1[i][4])
+        if float(list_1[i][3]) > 10:
+            minut_call += float(list_1[i][3])-10.0
+            coat_sms += float(list_1[i][4])
+        else:
+            coat_sms += float(list_1[i][4])
+        
+minut_call = round(minut_call, 2)
 
 out = open('output.txt', 'w')
-if coat_sms <= 10:
-    print('0', str(k*minut_call), file = out)
-else: 
-    print(str((coat_sms-10)*s), str(k*minut_call), file = out)
+print(str(coat_sms*s), str(k*minut_call), file = out)
 
 data_file.close
 out.close
